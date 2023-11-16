57cf1f2f09605ea9724ddac381728ef016c45e22
huyz <739820174@qq.com>
2023-11-17 00:14:12 +0800

    project init

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+
+const Chat = () => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser",
+    "1234"
+  );
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
+  );
+};
+
+export default Chat;
---
+++ client/vite.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
