cdadcd527695951c6543c8fce27993befe043486
huyz <739820174@qq.com>
2023-11-21 23:04:45 +0800

    1. 完善服务端开发 2. 完善客户端开发

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -5,13 +5,18 @@ import {
   MultiChatWindow,
 } from "react-chat-engine-advanced";
 import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
+import Ai from "@/components/customMessageForms/Ai";
+import AiCode from "@/components/customMessageForms/AiCode";
+import AiAssist from "@/components/customMessageForms/AiAssist";
 
-const Chat = () => {
+const Chat = ({ user, secret }) => {
   const chatProps = useMultiChatLogic(
     import.meta.env.VITE_PROJECT_ID,
-    "testuser",
-    "1234"
+    user,
+    secret
   );
+
   return (
     <div style={{ flexBasis: "100%" }}>
       <MultiChatSocket {...chatProps} />
@@ -19,6 +24,21 @@ const Chat = () => {
         {...chatProps}
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiAssist_")) {
+            return <AiAssist props={props} activeChat={chatProps.chat} />;
+          }
+
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
       />
     </div>
   );
